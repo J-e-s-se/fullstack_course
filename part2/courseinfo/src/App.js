ef205fae69cc3c9dde2ba554fb597b78ff1b9605
Jesse <oluguy911@gmail.com>
2022-05-11 13:21:54 +0100

    Exercise 2.5: separate module

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,34 +1,4 @@
-const Header = ({ course }) => <h1>{course}</h1>;
-
-const Total = ({ sum }) => <h3>total of  {sum} exercises</h3>;
-
-const Part = ({ part }) => (
-  <p>
-    {part.name} {part.exercises}
-  </p>
-);
-
-const Content = ({ parts }) => (
-  <>
-    {parts.map((part) => (
-      <Part key={part.id} part={part} />
-    ))}
-  </>
-);
-
-const Course = ({ course }) => {
-  const total = course.parts.reduce(
-    (accumulator, currentPart) => accumulator + currentPart.exercises,
-    0
-  );
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total sum={total} />
-    </div>
-  );
-};
+import Course from "./components/Course";
 
 const App = () => {
   const courses = [
