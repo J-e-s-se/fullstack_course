a28b9f726b215077ee804d29119c145a78e84668
Jesse <oluguy911@gmail.com>
2022-05-11 10:16:30 +0100

    Exercise 1.9: unicafe step4

--- part1/app/src/App.js
+++ part1/app/src/App.js
@@ -1,26 +1,32 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 const Statistics = (props) => {
-  const {good, neutral, bad} = props
-  const all = good + neutral + bad
+  const { good, neutral, bad } = props;
+  const all = good + neutral + bad;
   return (
     <div>
       <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {all}</p>
-      <p>average {(good - bad) / all}</p>
-      <p>positive {(good * 100) / all}</p>
+      {all > 0 ? (
+        <div>
+          <p>good {good}</p>
+          <p>neutral {neutral}</p>
+          <p>bad {bad}</p>
+          <p>all {all}</p>
+          <p>average {(good - bad) / all}</p>
+          <p>positive {(good * 100) / all}</p>
+        </div>
+      ) : (
+        <p>No feedback given</p>
+      )}
     </div>
-  )
-}
+  );
+};
 
 const App = () => {
   // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
   return (
     <div>
       <h1>give feedback</h1>
@@ -29,7 +35,7 @@ const App = () => {
       <button onClick={() => setBad(bad + 1)}>bad</button>
       <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
